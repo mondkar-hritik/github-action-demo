caece6d1e8de7cb62aea572d986bf8257e82a4d5
Hritik Mondkar <hritik.mondkar@atos.net>
2023-04-25 16:15:30 +0530

    initial commit

---
+++ src/test/java/com/hritik/spring/sample/app/SpringSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hritik.spring.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
